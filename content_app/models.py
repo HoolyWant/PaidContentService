489d24479e3e88911da23b8165430180f41b6ac7
hoolywant <sir.saltickow@yandex.ru>
2023-12-04 23:52:51 +0400

    models init

--- content_app/models.py
+++ content_app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Channel(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='владелец', **NULLABLE)
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.CharField()
+    image = models.ImageField(upload_to='channels/', verbose_name='изображение', **NULLABLE)
+    made_at = models.DateTimeField()
+    subscribers = models.IntegerField()
+    cost = models.PositiveIntegerField(default=0, verbose_name='сумма оплаты', **NULLABLE)
+    publications_count = ...
+
+
+class Publication(models.Model):
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, verbose_name='канал', **NULLABLE)
+    title = models.CharField()
+    content = models.TextField()
+    image = models.ImageField(upload_to='publications/', verbose_name='изображение', **NULLABLE)
+    is_free = models.BooleanField()
+    made_at = models.DateTimeField()
+    views_count = models.PositiveIntegerField(default=0, verbose_name='сумма оплаты', **NULLABLE)
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь', **NULLABLE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, verbose_name='канал', **NULLABLE)
+    subscription_status = models.BooleanField(default=False, verbose_name='статус подписки')
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
+
+    def __str__(self):
+        return f'{self.user}, {self.channel}, {self.subscription_status}'
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь', **NULLABLE)
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name='дата оплаты')
+    channel = models.ForeignKey(Channel,  on_delete=models.CASCADE, verbose_name='курс', **NULLABLE)
+    amount = models.PositiveIntegerField(default=0, verbose_name='сумма оплаты', **NULLABLE)
+    link = models.CharField(max_length=500, verbose_name='ссылка', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'оплата'
+        verbose_name_plural = 'оплаты'
+
+    def __str__(self):
+        return self.amount
--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-NULLABLE = {'blank': True, 'null': True}
+c
 
 
 class User(AbstractUser):
