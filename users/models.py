d7ec0ac8250d2f2659e420c0e4764ea7bf40645d
hoolywant <sir.saltickow@yandex.ru>
2023-12-04 12:31:38 +0400

    first migration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    phone_number = models.CharField(unique=True, verbose_name='номер телефона')
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone_number
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
